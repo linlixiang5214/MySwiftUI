05a6c24e6124a8832bd6d787f098e2391e658c42
linlixiang <924203689@qq.com>
2025-07-20 13:05:11 +0800

    Initial Commit

---
+++ MySwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MySwiftUI
+//
+//  Created by 林立祥 on 2025/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MySwiftUI/MySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUIApp.swift
+//  MySwiftUI
+//
+//  Created by 林立祥 on 2025/7/20.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
