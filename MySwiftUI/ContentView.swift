801da8b023fb48aabb67717dc79450cef5050e1a
linlixiang <924203689@qq.com>
2025-07-21 15:54:14 +0800

    update: 定时循环轮播UI实现

--- MySwiftUI/ContentView.swift
+++ MySwiftUI/ContentView.swift
@@ -8,21 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private var colors: [Color] = [.blue, .green, .orange, .purple, .pink, .brown]
+    
+    @State private var curColorStr: String = "0"
+    
     var body: some View {
-        MyCycleScanner(pageCount: 4){  index  in
-            RoundedRectangle(cornerRadius: 10)
+        Text("当前的颜色是: \(curColorStr)")
+        
+        let itemSize = CGSize(width: 220, height: 140)
+        MyCycleScanner(pageCount: colors.count, itemSize: itemSize, itemSpacing: 0, curColorStr: $curColorStr){  index  in
+            RoundedRectangle(cornerRadius: 20)
                 .fill(colors[index])
                 .overlay(Text("Page \(index + 1)")
                     .font(.title).bold()
-                    .foregroundColor(.blue))
-                .shadow(radius: 5)
+                    .foregroundColor(.white))
+                
+                
                 
         }
-        .config(itemSize: CGSize(width: 40, height: 20), itemSpacing: 0)
-        .frame(width: 120, height: 80)
-        .border(.blue)
+        .frame(width: 220, height: 160)
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+        .overlay {
+            RoundedRectangle(cornerRadius: 20).stroke(Color.blue, lineWidth: 1)
+        }
+        .environment(colors)
     }
+
     
-    private var colors: [Color] = [.blue, .green, .orange, .purple]
 }
 
--- MySwiftUI/CycleScanner.swift
+++ MySwiftUI/CycleScanner.swift
@@ -21,6 +21,8 @@ struct CycleScannerConfig {
 
 struct MyCycleScanner<Content: View>: View {
     
+    @Environment var color: [Color]
+    
     let pageCount: Int
     
     let content: (Int) -> Content
@@ -30,40 +32,89 @@ struct MyCycleScanner<Content: View>: View {
     @State private var currentIndex: Int = 0
     
     @State private var direction: Axis.Set = .horizontal
+    
+    @Binding var curStr: String
     /// item 大小
-    private var itemSize: CGSize = CGSize(width: 30, height: 30)
+    private var itemSize: CGSize = .zero
     /// item 间距
     private var itemSpacing: CGFloat = 0
     
-    init(pageCount: Int, @ViewBuilder content: @escaping (Int) -> Content) {
+    @State private var timer: Timer?
+    
+    init(pageCount: Int, itemSize: CGSize, itemSpacing: CGFloat, curColorStr: Binding<String>, @ViewBuilder content: @escaping (Int) -> Content) {
+        self._curStr = curColorStr
         self.pageCount = pageCount
         self.content = content
+        self.itemSize = itemSize
+        self.itemSpacing = itemSpacing
     }
     
     var body: some View {
-        Text("itemSize: \(itemSize)").font(.system(size: 8))
+        Text("itemSize: \(itemSize)").font(.system(size: 10)).frame(height: 20)
         let _ = print("body update")
-        ScrollView(direction) {
-            let totalWidth = (itemSize.width + itemSpacing) * CGFloat(pageCount)
-            HStack(alignment: .center, spacing: itemSpacing) {
-                ForEach(0..<pageCount, id: \.self) { index in
-                    content(index)
-                        .frame(width: itemSize.width, height: itemSize.height, alignment: .center)
+        
+        let scrollStep = direction == .horizontal ? itemSize.width: itemSize.height
+        let totalLength = (scrollStep + itemSpacing) * CGFloat(pageCount) - itemSpacing
+        
+        GeometryReader() { geo in
+            if direction == .horizontal {
+                HStack(alignment: .center, spacing: itemSpacing) {
+                    ForEach(0..<pageCount, id: \.self) { index in
+                        content(index)
+                            .frame(width: itemSize.width, height: itemSize.height, alignment: .center)
+                    }
                 }
+                .frame(width: totalLength)
+            } else {
+                VStack(alignment: .center, spacing: itemSpacing) {
+                    ForEach(0..<pageCount, id: \.self) { index in
+                        content(index)
+                            .frame(width: itemSize.width, height: itemSize.height, alignment: .center)
+                    }
+                }
+                .frame(height: totalLength)
             }
-            .frame(width: totalWidth)
         }
+        .offset(x: -(CGFloat(currentIndex) * (itemSize.width + itemSpacing)))
+        .onAppear(perform: startTimer)
+        .onDisappear(perform: stopTimer)
+        .gesture(
+            DragGesture()
+                .onChanged { value in
+                    stopTimer()
+                }
+                .onEnded { value in
+                    let translation = value.translation
+                    let offset = direction == .horizontal ? translation.width : translation.height
+                    let itemSize = direction == .horizontal ? itemSize.width + itemSpacing : itemSize.height + itemSpacing
+                    
+                    // 判断滑动方向
+                    if abs(offset) > itemSize / 2 {
+                        let newIndex = offset < 0 ? min(currentIndex + 1, pageCount - 1) : max(currentIndex - 1, 0)
+                        currentIndex = newIndex
+                    }
+                    startTimer()
+                }
+        )
         
     }
     
-    
-    func config(itemSize: CGSize = .zero, itemSpacing: CGFloat = 0) -> some View {
-        self.itemSize = itemSize
-        self.itemSpacing = itemSpacing
-        
-        print("config: \(itemSize)")
-        return self.transformEnvironment(\.self) { _ in }
+    private func startTimer() {
+        stopTimer()
+        timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: true) {  timer in
+            let index = (self.currentIndex + 1) % self.pageCount
+            withAnimation(index == 0 ? .none : .default) {
+                self.currentIndex = index
+            }
+            
+            print("timer invoke: \(index), pageCount:\(self.pageCount)")
+            self.curStr = "\(self.currentIndex)"
+        }
     }
     
+    private func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
     
 }
